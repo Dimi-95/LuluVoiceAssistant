d6b483d8ba63dc073cb085d46f0139503d6cce52
Dimi <46501122+DimisCode@users.noreply.github.com>
2021-01-06 11:14:22 +0100

    Add files via upload
    Code changed as far as letting it speak back to the user. This is where we hit the limitation of the Raspi as the PyObjC module can't be installed in this OS.
    
    additional modules that got added/imported are:
    
    playsound
    gTTS
    time( again, to make the program reset within 1sec)
    
    
    the print statements have been changed with the function "lulu_speak", which is currently not working due to the missing module PyObjC. Further tests will be done with Windows as the OS but as of now, the project on the RASPI is being limited to non speech.

--- test.py
+++ test.py
@@ -2,35 +2,53 @@ import speech_recognition as sr
 import pyaudio
 import webbrowser
 from time import ctime
+import playsound
+import os
+import random
+from gtts import gTTS
+import time
 
 r = sr.Recognizer()
 
 def record_audio(ask = False):
     with sr.Microphone() as source:
         if ask:
-            print(ask)
+            lulu_speak(ask)
         audio = r.listen(source)
         voice_data = ""
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            print("Sorry, I did not get that")
+            lulu_speak("Sorry, I did not get that")
         except sr.RequestError:
-            print("Sorry but the service is down")
+            lulu_speak("Sorry but the service is down")
         return voice_data
 
+def lulu_speak(audio_string):
+
+    tts = gTTS(text=audio_string, lang="en")
+    r = random.randint(1, 10000000)
+    audio_file = "audio-" + str(r) + ".mp3"
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
 def respond(voice_data):
-    if "what is your name" in voice_data:
-        print("My name is Lulu")
+   # if "what is your name" in voice_data:
+    #    lulu_speak("My name is Lulu")
     if "what time is it" in voice_data:
-        print(ctime())
+        lulu_speak(ctime())
     if "search" in voice_data:
         search = record_audio("What do you want to search for ?")
         url = "https://google.com/search?q=" + search
-        #https://duckduckgo.com/?q=lulu&t=raspberrypi&ia=web
+        
         webbrowser.get().open(url)
-        print("Here is what I found for " + search)
-    
-print("How can I help you ?")
-voice_data = record_audio()
-respond(voice_data)
+        lulu_speak("Here is what I found for " + search)
+
+
+time.sleep(1)
+lulu_speak("How can I help you ?")
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
